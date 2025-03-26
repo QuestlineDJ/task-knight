26c3536355d0fc18b4f3f0f1a350065be6d3f063
lilbbusa <bbostergames@gmail.com>
2025-03-26 17:11:49 -0500

    Added code from Health-Example

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { TaskMaster} from './TaskSystem'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { TaskMaster } from "./TaskSystem";
+import EnemyDamage from "./EnemyDamage";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-   <TaskMaster/>
-  </React.StrictMode>,
-)
+    <TaskMaster />
+    <EnemyDamage />
+  </React.StrictMode>
+);
