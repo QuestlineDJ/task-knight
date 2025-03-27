65f4c1415ae1075aa20b44d23c67ddc37d42d61e
ClareG2004 <cgrady@mail.bradley.edu>
2025-03-26 21:39:03 -0500

    Basic Ass task board

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Message from "./Message";
 import BackgroundGrid from "./ReactComponents/BackgroundGrid";
+
 import Background from "./assets/Task Knight Assets/Background Assets/background_plains.png";
 
 function App() {
   return (
-    <div>
+    <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
       <BackgroundGrid></BackgroundGrid>
     </div>
   );
--- src/ReactComponents/BackgroundGrid.tsx
+++ src/ReactComponents/BackgroundGrid.tsx
@@ -1,5 +1,6 @@
 import Background from "../assets/Task Knight Assets/Background Assets/background_plains.png";
 import Curtain from "../assets/Task Knight Assets/Background Assets/border_curtains.png";
+import taskBoard from "../assets/Task Knight Assets/Main Panel/Task Panel/panel_tasks.png";
 
 function BackgroundGrid() {
   return (
@@ -18,8 +19,21 @@ function BackgroundGrid() {
         }}
       ></img>
       <img
+        src={taskBoard}
+        alt="TaskBoard"
+        style={{
+          width: "69%",
+          height: "60%",
+          position: "absolute",
+          zIndex: 2,
+          top: "70%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      ></img>
+      <img
         src={Curtain}
-        alt="Background"
+        alt="Curtain"
         style={{
           width: "100%",
           height: "100%",
@@ -27,7 +41,7 @@ function BackgroundGrid() {
           position: "absolute",
           top: 0,
           left: 0,
-          zIndex: 1,
+          zIndex: 2,
         }}
       ></img>
     </div>
