3d9d8864702ba64be92f2dd65c47c6cf178a7383
kaptainKirby <rherwig@mail.bradley.edu>
2025-04-11 02:40:43 -0500

    Game Data and Save System Sync Partially

--- src/GameHelper.tsx
+++ src/GameHelper.tsx
@@ -1,4 +1,92 @@
-import { setLocalStorage, getAllLocalStorage } from "./LocalStorageManager";
+import { setLocalStorage, getAllLocalStorage, getAllMappedLocalStorage, getLocalStorage } from "./LocalStorageManager";
+
+export class GameData {
+  gold: number;
+  attack: number;
+  bossHealth: number;
+
+  /**
+   * @param gold The amount of gold the player has
+   * @param attack The amount of damage the player does per attack
+   * @param bossHealth The amount of health the boss has
+   */
+  constructor(gold: number, attack: number, bossHealth: number)
+  {
+    this.gold = gold;
+    this.attack = attack;
+    this.bossHealth = bossHealth;
+  }
+}
+
+export function saveGameDataToStorage(gameData: GameData) {
+   var storage = {
+      gold: gameData.gold,
+      attack: gameData.attack,
+      bossHealth: gameData.bossHealth
+   };
+
+   console.log(getKey(), JSON.stringify(storage));
+   setLocalStorage(getKey(), JSON.stringify(storage));
+}
+
+export function LoadGameDataFromStorage()
+{
+  var data = getAllMappedLocalStorage();
+
+   data.forEach((value, key) =>{
+      if ( key === getKey()) {
+         // Discard prefix
+         var data: GameData  = JSON.parse(value);
+
+         return new GameData(data.gold, data.attack, data.bossHealth);
+      }
+   });
+}
+
+function getKey() { return "game.data" };
+
+var gameData = new GameData(0, 10, 100);
+
+function setPlayerDamage(  damage: number,
+  setDamageAmount: React.Dispatch<React.SetStateAction<number>>,
+) {
+  setDamageAmount(damage);
+}
+
+function setGold(gold: number, setCurrentGoldAmount: React.Dispatch<React.SetStateAction<number>>)
+{
+  setCurrentGoldAmount(() => {
+    const newGold = gold;
+    console.log("Updated Gold:", newGold);
+    gameData.gold = newGold;
+    return newGold;
+  });
+}
+
+export function setBossHealth(
+  health: number,
+  currentImage: number,
+  images: string[],
+  setEnemyHealth: React.Dispatch<React.SetStateAction<number>>,
+  setCurrentImage: React.Dispatch<React.SetStateAction<number>>,
+  setCurrentGoldAmount: React.Dispatch<React.SetStateAction<number>>
+) {
+  setEnemyHealth((prev) => {
+    const newHealth = health;
+    console.log("Updated Health: ", newHealth);
+
+    if (newHealth === 0 && currentImage < images.length) {
+      setTimeout(() => {
+        setCurrentImage((prevIndex) => prevIndex + 1);
+        setEnemyHealth(100);
+        giveGold(setCurrentGoldAmount);
+      }, 500);
+    }
+    gameData.bossHealth = newHealth;
+    setLocalStorage(getKey(), JSON.stringify(gameData));
+    return newHealth;
+  });
+}
 
 export function increasePlayerDamage(
   currentGoldAmount: number,
@@ -13,7 +101,8 @@ export function increasePlayerDamage(
       "Increased damage by 10. New damage amount: ",
       damageAmount + 10
     );
-    //setLocalStorage("currentDamage", damageAmount as any as string);
+    gameData.attack = damageAmount;
+    setLocalStorage(getKey(), JSON.stringify(gameData));
   } else {
     console.log("Not enough gold");
   }
@@ -25,7 +114,8 @@ export function giveGold(
   setCurrentGoldAmount((prev) => {
     const newGold = Math.max(0, prev + 10);
     console.log("Updated Gold:", newGold);
-    //setLocalStorage("gold", newGold as any as string);
+    gameData.gold = newGold;
+    setLocalStorage(getKey(), JSON.stringify(gameData));
     return newGold;
   });
 }
@@ -49,7 +139,8 @@ export function damageEnemy(
         giveGold(setCurrentGoldAmount);
       }, 500);
     }
-    setLocalStorage("bossHealth", newHealth as any as string);
+    gameData.bossHealth = newHealth;
+    setLocalStorage(getKey(), JSON.stringify(gameData));
     return newHealth;
   });
 }
--- src/LocalStorageManager.tsx
+++ src/LocalStorageManager.tsx
@@ -14,6 +14,7 @@
  *
  * Author: Ryan Herwig
  */
+import { GameData, saveGameDataToStorage } from "./GameHelper";
 import { saveTaskToStorage, Task, TaskType, TaskStorage } from "./TaskUtilities";
 
 const IS_DEBUGGING = false;
@@ -71,26 +72,35 @@ export function deleteAllLocalStorage() {
 }
 
 
-var privateNonlocalData = "";
+var privateNonlocalTaskData = "";
+var privateNonlocalGameData = "";
 /**
  * Saves the local storage data into a txt file and downloads it onto the computer
  */
 export function saveFile() {
     //Gets data
     var dataMap = getAllMappedLocalStorage();
-    privateNonlocalData = "";
+    privateNonlocalTaskData = "";
+    privateNonlocalGameData = "";
 
     //Sends data into a string
     dataMap.forEach((value, key) => {
-        if (key.startsWith("active.")) {
+        if (key.startsWith("active.") || key.startsWith("complete.")) {
             // Discard prefix
             var taskid = Number(key.slice("active.".length));
             var dataValue: TaskStorage = JSON.parse(value);
 
-            privateNonlocalData += JSON.stringify(new Task(dataValue.name, dataValue.priority, dataValue.due_time, taskid));
+            privateNonlocalTaskData += JSON.stringify(new Task(dataValue.name, dataValue.priority, dataValue.due_time, taskid));
+        }
+        if (key === "game.data")
+        {
+            var dataVal: GameData = JSON.parse(value);
+
+            privateNonlocalGameData += JSON.stringify(new GameData(dataVal.gold, dataVal.attack, dataVal.bossHealth));
         }
     });
-    console.log(privateNonlocalData);
+    console.log(privateNonlocalTaskData);
+    console.log(privateNonlocalGameData);
 
     var data = encrypt();
     //Converts the data to a plain text Blob
@@ -139,24 +149,30 @@ export function loadFile(event: React.ChangeEvent<HTMLInputElement>) {
         console.log(data); //DEBUG - DELETE LATER
 
         //Creates an array of all tasks
-        var dataArray = new Array();
+        var taskDataArray = new Array();
+        
         //Splits tasks by the } bracket
-        var iterations = data.split('}').length - 1;
+        var taskData = data.substring(0, data.indexOf("|"));
+        var gameData = data.substring(data.indexOf("|") + 1, data.lastIndexOf("}") + 1);
+        var iterations = taskData.split('}').length - 1;
         for (var i = 0; i < iterations; i++) {
-            var index = data.indexOf('}');
+            var index = taskData.indexOf('}');
 
             //Creates a JSON string and pushes it to the array
-            dataArray.push(JSON.parse(data.substring(0, index + 1)));
+            taskDataArray.push(JSON.parse(taskData.substring(0, index + 1)));
 
-            data = data.substring(index + 1);
+            taskData = taskData.substring(index + 1);
         }
+
+        var gameDataArray = JSON.parse(gameData);
 
         //Creates tasks from the JSON strings
-        for (var i = 0; i < dataArray.length; i++) {
-            saveTaskToStorage(new Task(dataArray[i].name, dataArray[i].priority, dataArray[i].due_time,
-                dataArray[i].id, dataArray[i].creation_time, dataArray[i].done_time, 
-                dataArray[i].task_type), dataArray[i].task_type);
+        for (var i = 0; i < taskDataArray.length; i++) {
+            saveTaskToStorage(new Task(taskDataArray[i].name, taskDataArray[i].priority, taskDataArray[i].due_time,
+                taskDataArray[i].id, taskDataArray[i].creation_time, taskDataArray[i].done_time,
+                taskDataArray[i].task_type), taskDataArray[i].task_type);
         }
+        saveGameDataToStorage(new GameData(gameDataArray.gold, gameDataArray.attack, gameDataArray.bossHealth));
     };
 
     if (!fileInput.files) {
@@ -212,7 +228,9 @@ const saltAmountArray = [1, 2, 3, 4, 5];
 
 function encrypt() {
     //Gets the data from the storage
-    var plainText = privateNonlocalData;
+    var plainText = privateNonlocalTaskData;
+    plainText += "|";
+    plainText += privateNonlocalGameData;
     console.log("Plain Text: " + plainText);
 
     //Creates Vigenere Cipher using keyWord
