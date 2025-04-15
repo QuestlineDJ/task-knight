d16c92b711de7f118a7376447c6ff3d5b6354b6f
Quest <mary.kquest@gmail.com>
2025-04-14 21:47:45 -0500

    Initial CSS Updates

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+// Image imports (placeholders - replace with your own)
+import headerLeft from "./assets/Task Knight Assets/Corner UI/emptyLevelBadge.png";
+import headerRight from "./assets/Task Knight Assets/Corner UI/goldBag.png";
+
+import character from "./assets/Task Knight Assets/Character UI & Sprites/playerCharacter_default.png";
+import dragon from "./assets/Task Knight Assets/Character UI & Sprites/boss_dragon.png";
+
+import panelBg from "./assets/Task Knight Assets/Main Panel/Task Panel/panel_tasks.png";
+import listItemBg from "./assets/Task Knight Assets/Main Panel/Task Panel/taskBackground.png";
+
+import btnUpDown from "./assets/Task Knight Assets/Main Panel/Task Panel/sortButton.png";
+import btnAdd from "./assets/Task Knight Assets/Main Panel/Task Panel/addButton.png";
+
+const tasks: string[] = [
+  "Take Out the Trash",
+  "Clean Container",
+  "Replace Bag",
+  "Finish Doing Laundry",
+  "Finish Math Homework",
+];
+
+const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <header className="header">
+        <img src={headerLeft} alt="Left Header" />
+        <img src={headerRight} alt="Right Header" />
+      </header>
+
+      <div className="main-content">
+        <aside className="sidebar left-sidebar" />
+
+        <section className="content">
+          <div className="game-scene">
+            <img src={character} alt="Character" className="character" />
+            <img src={dragon} alt="Dragon" className="dragon" />
+          </div>
+
+          <div className="ui-overlay">
+            <img src={panelBg} alt="Panel Background" className="panel-bg" />
+            <div className="task-list">
+              {tasks.map((task, index) => (
+                <div key={index} className="task-item">
+                  <img
+                    src={listItemBg}
+                    alt="Task Background"
+                    className="list-item-bg"
+                  />
+                  <span className="task-text">{task}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+
+        <aside className="sidebar right-sidebar">
+          <img src={btnUpDown} alt="Reorder Button" className="button-updown" />
+          <img src={btnAdd} alt="Add Task Button" className="button-add" />
+        </aside>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { TaskMaster } from "./TaskSystem";
-import "./main.css";
-import SaveWindow from "./Components/SaveWindow";
-import ShopTab from "./Components/ShopTab";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="Container">
-      <div className="main">
-        <SaveWindow />
-      </div>
-
-      <div className="tasks">
-        <TaskMaster />
-      </div>
-    </div>
+    <App />
   </React.StrictMode>
 );
-
-const beforeUnloadHandler = (event: any) => {
-  // Recommended
-  event.preventDefault();
-
-  // Included for legacy support, e.g. Chrome/Edge < 119
-  event.returnValue = true;
-};
-
-window.addEventListener("beforeunload", beforeUnloadHandler);
